5809885a1b2326f5d32aac06b09e3f9cd6db42b4
Basavaraj HM <basavaraj.hm@lowes.com>
2020-02-22 21:28:37 +0530

    feat: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Person from './Person/Person'
+
+const App = () => {
+  return (
+    <div>
+      <Person name='Basavaraj HM' age='30' />
+      <Person name='Naveen N' age='30' />
+    </div>
+  );
+}
+
+export default App;
